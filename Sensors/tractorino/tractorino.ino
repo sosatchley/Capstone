e6b0306e1c97cb18ef3402c9413d8234604e0611
Unknown <sosatchley@gmail.com>
2019-10-12 10:30:13 -0500

    Tractor Vision
    Masking Wheel

--- Sensors/tractorino/tractorino.ino
+++ Sensors/tractorino/tractorino.ino
@@ -1,56 +1,75 @@
-/* Learn how to use a potentiometer to fade an LED with Arduino - Tutorial
-   More info and circuit schematic: http://www.ardumotive.com/arduino-tutorials/arduino-fade-led
-   Dev: Michalis Vasilakis / Date: 25/10/2014                                                   */
-
 
 //Constants:
-const int ledPin = 13;  //pin 9 has PWM funtion
-const int potPin = A0; //pin A0 to read analog input
-const int L1 = A1;
-const int L2 = A2;
-const int L3 = A3;
+const int potPin = A0;
+const int encoderL1 = A1;
+const int encoderL2 = A2;
+const int encoderL3 = A3;
 const unsigned char PS_128 = (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
 
 //Variables:
-int value; //save analog value
+int inByte = 0;
+int potValue = 0;
+int lastPotValue;
+int encoderValue = 0;
+int encoderDirection = 0;
 boolean L1Val = 0;
 
-
 void setup(){
-  //Input or output?
-  pinMode(ledPin, OUTPUT);
-  pinMode(potPin, INPUT); //Optional
-  // Serial.begin(9600);
+  Serial.begin(9600);
   ADCSRA &= ~PS_128;
   ADCSRA |= (1 << ADPS1) | (1 << ADPS0);
-  Serial.begin(115200);
   analogReference(INTERNAL);
-  pinMode(L1, INPUT);
-  pinMode(L2, INPUT);
-  pinMode(L3, INPUT);
-
+  pinMode(potPin, INPUT);
+  pinMode(encoderL1, INPUT);
+  pinMode(encoderL2, INPUT);
+  pinMode(encoderL3, INPUT);
+  establishContact();
 }
 
-void loop(){
-
-  value = analogRead(potPin);          //Read and save analog value from potentiometer
-  value = map(value, 0, 1023, 0, 255); //Map value 0-1023 to 0-255 (PWM)
-  Serial.println(value);
-  analogWrite(ledPin, value);          //Send PWM value to led
-  if ((analogRead(L1) > 200) && (L1Val == 0)) {
-    L1Val = 1;
+void loop() {
+  // if (Serial.available() > 0) {
+  //   inByte = Serial.read();
+    lastPotValue = potValue;
+    potValue = analogRead(potPin);
+    potValue = map(potValue, 0, 1023, 0, 255);
+    // encoderValue = analogRead(encoderL1);
+    //
+    // if ((encoderValue > 200) && (L1Val == 0)) {
+    //   L1Val = 1;
+    //   delay(100);
+    //   // Serial.write(analogRead(encoderL1));
+    //   // Serial.write(analogRead(encoderL2));
+    //   // Serial.write(analogRead(encoderL3));
+    // }
+    // if ((L1Val == 1) && (analogRead(encoderL2) > 200)) {
+    //   // Serial.println("Counter-Clockwise");
+    //   encoderDirection = 1;
+    //   L1Val = 0;
+    //   delay(100);
+    // }
+    // if ((L1Val == 1) && (analogRead(encoderL3) > 200)) {
+    //   // Serial.println("Clockwise");
+    //   encoderDirection = 0;
+    //   L1Val = 0;
+    //   delay(100);
+    // }
+    // Serial.write(potValue);
+    // Serial.write(encoderValue);
+    // Serial.println(encoderDirection);
+  // }
+  // Serial.print(analogRead(encoderL1));
+  // Serial.print("\t");
+  // Serial.print(analogRead(encoderL2));
+  // Serial.print("\t");
+  // Serial.println(analogRead(encoderL3));
+  if (potValue != lastPotValue) {
+    Serial.write(potValue);
   }
+}
 
-  if ((L1Val == 1) && (analogRead(L2) > 200)) {
-    Serial.println("Counter-clockwise");
-    L1Val = 0;
-    delay(100);
+void establishContact() {
+  while (Serial.available() <= 0) {
+    Serial.write('A');
+    delay(300);
   }
-
-  if ((L1Val == 1) && (analogRead(L3) > 200)) {
-    Serial.println("Clockwise");
-    L1Val = 0;
-    delay(100);
-  }
-
 }
